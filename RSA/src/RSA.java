eb6aae860071c001be1f6b482402fdd751e86cc0
root <root@pedro-ubuntu.(none)>
2014-03-11 19:25:04 +0000

    Finish exp by square rsa

--- RSA/src/RSA.java
+++ RSA/src/RSA.java
@@ -14,15 +14,7 @@ public class RSA {
 		
 		return result;
 		}
-	
-	/*public  BigInteger modPowSquare(String msg, BigInteger exp, BigInteger n){
-		rodrigo.jm.lourenco@gmail.com
-		githup
-		BigInteger convert = new BigInteger(msg);
-		BigInteger result=convert;
-		return 
-	}*/
-	
+
 	private BigInteger expBySquare(BigInteger x, BigInteger n){
 		
 		if(n.intValue()< 0) 
@@ -31,8 +23,14 @@ public class RSA {
 			return BigInteger.ONE;
 		else if(n.intValue()==1)
 			return x;
-		//else if 
-		
+		else if(n.divide(BigInteger.valueOf(2)).intValue() == 0) //even
+			return expBySquare(x.multiply(x), n.divide(BigInteger.valueOf(2)));
+		else return x.multiply(expBySquare(x.multiply(x), n.subtract(BigInteger.ONE).divide(BigInteger.valueOf(2))));
+	}
+	
+	private void showTime(BigInteger value, long rsaStartTime){
+		System.out.println("RSA value: " + value);
+		System.out.println("Total Time:  " + (System.nanoTime() - rsaStartTime));
 	}
 	
 	public static void main(String[] args){
@@ -41,10 +39,22 @@ public class RSA {
 		BigInteger p = new BigInteger("400");
 		BigInteger n = new BigInteger("2047");
 		BigInteger msg = new BigInteger("1024");
-		long rsaStartTime = System.nanoTime();
-		System.out.println("RSA value: " + rsa.modPow("1024", p, n) + " FinalTime: " + (System.nanoTime() - rsaStartTime));
-		rsaStartTime = System.nanoTime();
-		System.out.println("RSA bIvalue: " + msg.modPow(p, n)+ " FinalTime: " + (System.nanoTime() - rsaStartTime));
+	
 		
+		System.out.println("ModPow:");
+		long rsaStartTime = System.nanoTime();
+		BigInteger value = rsa.modPow("1024", p, n);
+		rsa.showTime(value, rsaStartTime);
+		
+		System.out.println("RSA bIvalue: ");
+		rsaStartTime = System.nanoTime();
+		value=msg.modPow(p, n);
+		rsa.showTime(value, rsaStartTime);
+		
+		System.out.println("RSA exponential by square: ");
+		rsaStartTime = System.nanoTime();
+		value=rsa.expBySquare(msg, p);
+		rsa.showTime(value, rsaStartTime);
 	}
+	
 }
