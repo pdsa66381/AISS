cd3085369f52b6305a62113ccc6d30c1fec6edea
pdsa66381 <pedrodsantunes@gmail.com>
2014-03-14 17:26:50 +0000

    Changes in squarepow function

--- RSA/src/RSABigInteger/SquarePow.java
+++ RSA/src/RSABigInteger/SquarePow.java
@@ -6,8 +6,16 @@ public class SquarePow implements PowerStrategy{
 
 	@Override
 	public BigInteger pow(BigInteger x, int e) {
-
-		return null;
+		
+		if(e < 0) 
+			return pow(BigInteger.ONE.divide(x), -e);
+		else if(e == 0)
+			return BigInteger.ONE;
+		else if(e == 1)
+			return x;
+		else if((e%2) == 0) //even
+			return pow(x.multiply(x), (e/2));
+		else return x.multiply(pow(x.multiply(x), ((e-1)/2))); //odd
 	}
 	
 	@Override
