859c90f21b7c0e8bf878683c4eb7290c99a13734
Rodrigo Lourenço <samm@sammcrunch.(none)>
2014-03-16 21:24:42 +0000

    Possivel solucao para mascarar info susceptivel a timming attacks

---
+++ RSA/src/NanoStopWatch.java
@@ -0,0 +1,22 @@
+/**
+ * Stop watch with nano precision 
+ */
+public class NanoStopWatch {
+
+	private double start_time = 0;
+	private double elapsed_time = 0;
+	
+	public void startClock(){
+		this.start_time = System.nanoTime();
+	}
+	
+	public double stopClock(){
+		
+		if(this.start_time>0){
+			this.elapsed_time = System.nanoTime() - this.start_time;
+			this.start_time = 0;
+			return this.elapsed_time;
+		}else
+			return -1;
+	}
+}
